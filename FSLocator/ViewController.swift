0929e1193ec402e7aa32812b8b2fa8ac54b2278b
Misnikov Roman <misnikovroman@gmail.com>
2019-12-14 20:26:29 +0300

    Создал начальный проект

---
+++ FSLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FSLocator
+//
+//  Created by MisnikovRoman on 14.12.2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
